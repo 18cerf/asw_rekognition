569d064e8207b2b8b6968183b056f6b3efdd3b77
18cerf <cerfscode@gmail.com>
2022-12-18 22:10:36 +0300

    Initial commit

---
+++ src/main/java/com/prizmarine/demo_asw_rekognition/DemoAswRekognitionApplication.java
@@ -0,0 +1,13 @@
+package com.prizmarine.demo_asw_rekognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAswRekognitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAswRekognitionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prizmarine/demo_asw_rekognition/DemoAswRekognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prizmarine.demo_asw_rekognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAswRekognitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
